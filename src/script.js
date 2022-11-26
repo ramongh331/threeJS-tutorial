efb24592e0ce789737d1beb68884ab5018b71272
Ramon Hernandez <ramongh331@gmail.com>
2022-11-26 18:25:10 -0500

    end of tutorial

--- src/script.js
+++ src/script.js
@@ -32,35 +32,101 @@ material.color = new THREE.Color(0x292929);
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-// Lights
+////////////////////////
+// LIGHTS LIGHTS LIGHTS
+////////////////////////
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
-
-const pointLight2 = new THREE.PointLight(0xff0000, 2);
+///////////////////
+// Light 1
+///////////////////
+const pointLight1 = new THREE.PointLight(0xff0000, 1);
 // pointLight.position.x = 2;
 // pointLight.position.y = 3;
 // pointLight.position.z = 4;
-pointLight2.position.set(1, 2, 1);
-pointLight2.intensity = 1;
+pointLight1.position.set(-1.39, 0.93, -0.59);
+pointLight1.intensity = 10;
+
+scene.add(pointLight1);
+// Gui Settings for Light 1
+/////////////////////////////
+// .addFolder creates the dropdown header for the gui.
+// .add in the beginning adds attributes to that specific dropdown
+const pointLight1Options = gui.addFolder("Light 1");
+pointLight1Options.add(pointLight1.position, "x").min(-3).max(3).step(0.01);
+pointLight1Options.add(pointLight1.position, "y").min(-3).max(3).step(0.01);
+pointLight1Options.add(pointLight1.position, "z").min(-3).max(3).step(0.01);
+pointLight1Options.add(pointLight1, "intensity").min(0).max(10).step(0.01);
+
+// change color
+/////////////////////////////
+const colorChange1 = {
+  color: 0xff0000,
+};
+pointLight1Options.addColor(colorChange1, "color").onChange(() => {
+  pointLight1.color.set(colorChange1.color);
+});
+
+// // the second parameter might be the size of the pointlight.
+// const pointLight1Helper = new THREE.PointLightHelper(pointLight1, 1);
+// scene.add(pointLight1Helper);
+
+///////////////////
+// Light 2
+///////////////////
+const pointLight2 = new THREE.PointLight(0x0000ff, 2);
+pointLight2.position.set(1.39, 0.93, -0.59);
+pointLight2.intensity = 10;
 
 scene.add(pointLight2);
 
-const pointLight3 = new THREE.PointLight(0x0000ff, 2);
-// pointLight.position.x = 2;
-// pointLight.position.y = 3;
-// pointLight.position.z = 4;
-pointLight3.position.set(1, -2, 1);
-pointLight3.intensity = 1;
+// Gui Settings for Light 2
+/////////////////////////////
+const pointLight2Options = gui.addFolder("Light 2");
+pointLight2Options.add(pointLight2.position, "x").min(-3).max(3).step(0.01);
+pointLight2Options.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
+pointLight2Options.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+pointLight2Options.add(pointLight2, "intensity").min(0).max(10).step(0.01);
+
+// change color
+/////////////////////////////
+const colorChange2 = {
+  color: 0x0000ff,
+};
+pointLight2Options.addColor(colorChange2, "color").onChange(() => {
+  pointLight2.color.set(colorChange2.color);
+});
+// const pointLight2Helper = new THREE.PointLightHelper(pointLight2, 1);
+// scene.add(pointLight2Helper);
+
+///////////////////
+// Light 3
+///////////////////
+const pointLight3 = new THREE.PointLight(0x00ff00, 2);
+pointLight3.position.set(0, -5.5, -2.44);
+pointLight3.intensity = 2.7;
 
 scene.add(pointLight3);
 
-gui.add(pointLight2.position, "x").min(-3).max(3).step(0.01);
-gui.add(pointLight2.position, "y").min(-3).max(3).step(0.01);
-gui.add(pointLight2.position, "z").min(-3).max(3).step(0.01);
+// Gui Settings for Light 3
+/////////////////////////////
+const pointLight3Options = gui.addFolder("Light 3");
+pointLight3Options.add(pointLight3.position, "x").min(-3).max(3).step(0.01);
+pointLight3Options.add(pointLight3.position, "y").min(-6).max(3).step(0.01);
+pointLight3Options.add(pointLight3.position, "z").min(-3).max(3).step(0.01);
+pointLight3Options.add(pointLight3, "intensity").min(0).max(10).step(0.01);
+// change color
+/////////////////////////////
+const colorChange3 = {
+  color: 0x00ff00,
+};
+pointLight3Options.addColor(colorChange3, "color").onChange(() => {
+  pointLight3.color.set(colorChange3.color);
+});
+
+// const pointLight3Helper = new THREE.PointLightHelper(pointLight3, 1);
+// scene.add(pointLight3Helper);
+//based on the gui settings in the window, I can now go up to the settings in my document and change those.
+// Use the gui as a way to experiment. You will not need the gui when you deploy your website.
 
 /**
  * Sizes
@@ -117,14 +183,43 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
+// Set up the Mouse Movement
+const onDocumentMouseMove = (event) => {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+};
+
+document.addEventListener("mousemove", onDocumentMouseMove);
+let mouseX = 0;
+let mouseY = 0;
+
+let targetX = 0;
+let targetY = 0;
+
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+///////////////////////
+const updateSphere = (event) => {
+  sphere.position.y = window.scrollY * 0.001;
+};
+
+window.addEventListener("scroll", updateSphere);
 
 const clock = new THREE.Clock();
 
 const tick = () => {
+  targetX = mouseX * 0.001;
+  targetY = mouseY * 0.001;
+
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
   sphere.rotation.y = 0.5 * elapsedTime;
+
+  // interrupts the rotation animation and follows the mouse movement.
+  sphere.rotation.x += 0.5 * (targetY - sphere.rotation.x);
+  sphere.rotation.y += 0.5 * (targetX - sphere.rotation.y);
+  sphere.position.z += 0.5 * (targetY - sphere.rotation.x);
 
   // Update Orbital Controls
   // controls.update()
